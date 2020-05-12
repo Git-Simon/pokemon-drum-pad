ff7c94fbd59150f877bf8fd3cdd4ecb8f0322f50
Simon <simon.aich@hetic.net>
2020-05-12 23:31:40 +0200

    reindent

--- script.js
+++ script.js
@@ -1,36 +1,31 @@
-function playSound(e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+function playSound(e) 
+{
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`) // attribute selector for the audio where there is a data key
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`) // select the corresponding key
 
-  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) return // stop the function from running all together
+  
+  key.classList.add('playing') // add the class playing for the transition
 
-
-  if (!audio) return;
-
-  key.classList.add('playing');
-
-  audio.currentTime = 0;
-  audio.play();
+  audio.currentTime = 0 // rewind sound from the start 
+  audio.play() // play sound linked with data key
 }
 
-function removeTransition(e) {
-
-  e.target.classList.remove('playing');
-
+function removeTransition(e)
+{
+  e.target.classList.remove('playing')
 }
 
+const keys = Array.from(document.querySelectorAll('.key')) // create an array that select every single key
+keys.forEach(key => key.addEventListener('transitionend', removeTransition)) // listen to all elements in the array when that end let run removeTransition
 
-const keys = Array.from(document.querySelectorAll('.key'));
-
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-
-
-window.addEventListener('keydown', playSound);
+window.addEventListener('keydown', playSound) // listen to the keydown events when that happened let the function run
 
 
-
-//toggle darkmode
-var button = document.getElementsByTagName("button")[0];
-var body = document.body;
-button.addEventListener("click", function () {
-  body.classList.toggle("dark-mode");
-});
+// toggle dark theme
+var button = document.getElementsByTagName("button")[0]
+var body = document.body
+button.addEventListener("click", () =>
+{
+  body.classList.toggle("dark-theme")
+})
