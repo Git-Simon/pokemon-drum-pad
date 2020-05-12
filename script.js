e78cd775d1302de7af86c18b40a7f7abb76bc4ea
Simon <simon.aich@hetic.net>
2020-05-12 17:50:06 +0200

    reindent

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
 
 
-    if (!audio) return;
+  if (!audio) return;
 
-    key.classList.add('playing');
+  key.classList.add('playing');
 
-    audio.currentTime = 0;
-    audio.play();
+  audio.currentTime = 0;
+  audio.play();
 }
 
 function removeTransition(e) {
 
-    e.target.classList.remove('playing');
+  e.target.classList.remove('playing');
 
 }
 
@@ -29,10 +29,8 @@ window.addEventListener('keydown', playSound);
 
 
 //toggle darkmode
-  var button = document.getElementsByTagName("button")[0];
-  var body = document.body;
-  button.addEventListener("click", function(){
-   body.classList.toggle("dark-mode");
-}); 
-
-//volume controller
+var button = document.getElementsByTagName("button")[0];
+var body = document.body;
+button.addEventListener("click", function () {
+  body.classList.toggle("dark-mode");
+});
