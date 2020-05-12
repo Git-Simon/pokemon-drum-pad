3d31915e8245acd50796da568ba80198651673f3
Simon <simon.aich@hetic.net>
2020-05-12 02:13:51 +0200

    test volume

--- script.js
+++ script.js
@@ -27,3 +27,12 @@ keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
 
 window.addEventListener('keydown', playSound);
+
+// volume
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
